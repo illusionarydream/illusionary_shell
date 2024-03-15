1682183f0b35ec8d1d4bd9bbff204bfc073ccfeb
illusionarydream <illusionary@sjtu.edu.cn>
2024-03-15 11:14:28 +0800

    new

--- main.c
+++ main.c
@@ -34,13 +34,38 @@ int execute_command(int client_socket, char *segment_array[], int n) {
         int command_length = parseLine(segment_array[0], command_array);
 
         // ?debug
-        for (int j = 0; j < command_length; j++) {
-            printf("Command %d: %s\n", j, command_array[j]);
-        }
-
+        // for (int j = 0; j < command_length; j++) {
+        //     printf("Command %d: %s\n", j, command_array[j]);
+        //     printf("Command %d: %d\n", j, strlen(command_array[j]));
+        // }
         // *execute the exit command
         if (strcmp(command_array[0], "exit") == 0) {
             return 0;
+        }
+
+        // *execute the command that change the directory
+        if (strcmp(command_array[0], "cd") == 0 && command_length == 2) {
+            // *change the directory to the home directory
+            for (const char *p = command_array[1]; *p != '\0'; ++p) {
+                if (*p == '~') {
+                    char *home = getenv("HOME");
+                    char *new_path = (char *)malloc(strlen(home) + strlen(p) + 1);
+                    strcpy(new_path, home);
+                    strcat(new_path, p + 1);
+                    if (chdir(new_path) == -1) {
+                        char *error_message = "Error: changing the directory\n";
+                        send(client_socket, error_message, strlen(error_message), 0);
+                    }
+                    return 1;
+                }
+            }
+
+            // *change the directory to the specified directory
+            if (chdir(command_array[1]) == -1) {
+                char *error_message = "Error: changing the directory\n";
+                send(client_socket, error_message, strlen(error_message), 0);
+            }
+            return 1;
         }
 
         // *create a child process
@@ -53,7 +78,8 @@ int execute_command(int client_socket, char *segment_array[], int n) {
 
             case 0:
                 dup2(client_socket, STDOUT_FILENO);
-                // *execute the command
+
+                // *execute the command normally
                 if (execvp(command_array[0], command_array) == -1) {
                     printf("Error: executing the command: %s\n", segment_array[0]);
                 }
@@ -139,10 +165,16 @@ int execute_command(int client_socket, char *segment_array[], int n) {
     return 1;
 }
 
-void handle_client(int client_socket) {
+void handle_line(int client_socket) {
     // *read and write data
     char buffer[1024];
     while (1) {
+        // *present the current directory
+        char cwd[1024];
+        getcwd(cwd, sizeof(cwd));
+        write(client_socket, cwd, strlen(cwd));
+        write(client_socket, " $ ", 3);
+
         // *initialize the buffer
         memset(buffer, 0, sizeof(buffer));
 
@@ -157,7 +189,6 @@ void handle_client(int client_socket) {
 
         // *remove the newline character
         int tmp_len = strlen(buffer);
-        printf("Client: %d\n", tmp_len);
         buffer[tmp_len - 1] = '\0';
         buffer[tmp_len - 2] = '\0';
 
@@ -173,9 +204,9 @@ void handle_client(int client_socket) {
         }
 
         // ?debug
-        for (int j = 0; j < i; j++) {
-            printf("Segment %d: %s\n", j, segment_array[j]);
-        }
+        // for (int j = 0; j < i; j++) {
+        //     printf("Segment %d: %s\n", j, segment_array[j]);
+        // }
 
         // *execute the command
         int if_exit = execute_command(client_socket, segment_array, i);
@@ -188,9 +219,39 @@ void handle_client(int client_socket) {
         // // *close the socket
         // close(client_socket);
     }
+    exit(0);
 }
 
-int main() {
+void handle_client(int client_socket) {
+    // *fork a child process to handle the new client
+    pid_t pid = fork();
+    if (pid == -1) {
+        // *handle the error
+        printf("Error: creating a child process\n");
+        exit(0);
+    }
+
+    // *child process
+    if (pid == 0) {
+        // *handle the client
+        handle_line(client_socket);
+    }
+
+    // *parent process
+    close(client_socket);
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    // *handle no arguments
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 0;
+    }
+
+    // *parse the port
+    int port = atoi(argv[1]);
+
     // *build a socket
     int network_socket;
     network_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -198,7 +259,7 @@ int main() {
     // *bind the socket to an IP address and port
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(12003);
+    server_address.sin_port = htons(port);
     inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
     bind(network_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
@@ -213,9 +274,14 @@ int main() {
         int len = sizeof(client_address);
         client_socket = accept(network_socket, (struct sockaddr *)&client_address, &len);
 
+        // *handle the error or disconnect
+        if (client_socket == -1)
+            continue;
+
         // *print out the client's IP address
         printf("Connection established\n");
         printf("Client IP: %s\n", inet_ntoa(client_address.sin_addr));
+        write(client_socket, "Welcome to the illusionary shell\n", 33);  // welcome message
 
         // *handle the client
         handle_client(client_socket);
